4d947e522b1b36a6bd87251bc5570f0ac3bfc6f1
VaidaEG <grausliene.vaida@gmail.com>
2021-03-21 16:42:07 +0200

    Gates update

--- resources/views/website/gates.blade.php
+++ resources/views/website/gates.blade.php
@@ -1,7 +1,10 @@
 @extends('layout.main')
 @section('content')
-<h1>Gates to website!</h1>
-{{$first}} --- {{$id}}
-
+    <h1>Gates to website!</h1>
+    @foreach(range(1,5) as $key => $value)
+        @if($id > 55)
+            {{$first}} --- {{$id}}
+        @endif
+    @endforeach
 @endsection
 @section('title') My website gates @endsection
